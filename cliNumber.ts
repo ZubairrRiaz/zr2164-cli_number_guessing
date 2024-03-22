16a2a5aac2fcdfebc19ff6b0c524e5c9459ce488
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-22 13:01:13 -0700

    Add files via upload

---
+++ cliNumber.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: 'userGuessNumber',
+        type: 'number',
+        message: 'Please Guess a number between 1 to 6;',
+
+    },
+
+]);
+
+if(answer.userGuessNumber === randomNumber){
+
+    console.log('Congratulations! you guessed a right number,');
+    
+}else {
+
+    console.log('You guessed wrong number,');
+    
+};
+
